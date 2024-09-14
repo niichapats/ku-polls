2af25b7d3b7de125abdcb3461dcd7c3665c2d2e7
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-09-14 12:02:34 +0700

    Edit ALLOWED_HOST

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda x: [i.strip() for i in x.split(',')], default='localhost')
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")
 
 # Application definition
 
